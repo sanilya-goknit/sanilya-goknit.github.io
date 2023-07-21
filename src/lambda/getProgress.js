ea4f6695876f9528d623c67d4628f735ac6364fa
Sanilya <panchalsanilya@gmail.com>
2023-07-21 10:30:56 +0530

    update

--- src/lambda/getProgress.js
+++ src/lambda/getProgress.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const getProgress = async (id) => {
     try {
-        const result = await axios.get(`http://localhost:3002/api/lambda/progress`, { params: { id } });
+        const result = await axios.get(`http://localhost:3002/showreel/getProgress`, { params: { id } });
         return result.data;
     }
     catch {
--- src/lambda/postMedia.js
+++ src/lambda/postMedia.js
@@ -2,7 +2,7 @@ import axios from "axios";
 
 export const postMedia = async (id, inputProps) => {
   try {
-    const result = await axios.post(`http://localhost:3002/api/lambda/media`, { id, inputProps });
+    const result = await axios.post(`http://localhost:3002/showreel/render`, { id, inputProps });
     return result.data;
   } catch {
     return null;
